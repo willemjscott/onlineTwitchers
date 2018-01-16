1a683eb6235dbce17ee552e0ad7e50d874f9b0ae
Will <willscott@Wills-MacBook-Pro.local>
2018-01-16 15:40:28 +1300

    finished

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 var twitchers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
 $(document).ready(function () {
     for (i = 0; i < twitchers.length; i++) {
-        getUserInfo(i);
+        getInfo(i);
     };
 
-    function getUserInfo(num) {
-        $.getJSON("https://wind-bow.gomix.me/twitch-api/users/" + twitchers[num] + "?callback=?", function (json) {
-            $("#channel-info-" + (num + 1)).html('<img src="' + json.logo + '" class="profile-pic">' + json.display_name);
+    function getInfo(num) {
+        $.getJSON("https://wind-bow.gomix.me/twitch-api/channels/" + twitchers[num] + "?callback=?", function (json) {
+            $("#channel-info-" + (num + 1)).html('<img src="' + json.logo + '" class="profile-pic"><a href="' + json.url + '">' + json.display_name + "</a>");
+        });
+        $.getJSON("https://wind-bow.gomix.me/twitch-api/streams/" + twitchers[num] + "?callback=?", function (json) {
+            var streaming;
+            if (json.stream === null) {
+                streaming = "Offline";
+            } else {
+                streaming = '<span title="' + json.stream.channel.status + '">Online</span>';
+                $("#stream-status-" + (num + 1)).css("color", "green")
+            };
+            $("#stream-status-" + (num + 1)).html(streaming);
         });
     };
 });
